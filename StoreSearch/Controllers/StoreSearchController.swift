a46e255af8ac23285332aa264e14efa2d59cacdb
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-12-28 19:27:10 +0500

    Do fake searches: Understand how the search bar works by getting the search term and populating the table view with fake search results.

--- StoreSearch/Controllers/StoreSearchController.swift
+++ StoreSearch/Controllers/StoreSearchController.swift
@@ -9,14 +9,55 @@ import UIKit
 
 class StoreSearchController: UIViewController {
    
+   //MARK: Properties
    @IBOutlet weak var searchBar: UISearchBar!
    @IBOutlet weak var tableView: UITableView!
    
+   
+   //MARK: Properties
+   var searchResult = [String]()
+   
+   //MARK: Initialzation
    override func viewDidLoad() {
       super.viewDidLoad()
       
    }
    
+   //MARK: Actions
+   
    
 }
 
+//MARK: - UISearchBarDelegate
+extension StoreSearchController: UISearchBarDelegate {
+   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+      searchResult = []
+      for i in 0..<3 {
+         searchResult.append(String(format: "Fake search %d for %@", i, searchBar.text!))
+      }
+      searchBar.resignFirstResponder()
+      tableView.reloadData()
+   }
+   
+   func position(for bar: any UIBarPositioning) -> UIBarPosition {
+      return .topAttached
+   }
+}
+
+//MARK: - UITableViewDataSource
+extension StoreSearchController: UITableViewDataSource {
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return searchResult.count
+   }
+   
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "SearchResultCell", for: indexPath)
+      cell.textLabel?.text = searchResult[indexPath.row]
+      return cell
+   }
+}
+
+//MARK: - UITableViewDelegate
+extension StoreSearchController: UITableViewDelegate {
+   
+}
